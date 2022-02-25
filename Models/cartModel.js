3ae256fd263f89fd7da62986a158ef6afa2cf787
romisaa-samir <romisaa.samir.mahmoud@gmail.com>
2022-02-25 17:17:49 +0200

    add cart model

---
+++ Models/cartModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const autoIncrement = require("mongoose-sequence")(mongoose);
+
+const schema = new mongoose.Schema(
+	{
+		_id: Number,
+        total_price :{type:Number, required: true }, 
+        product: [{type:Number,ref:"product"}],
+        user :{type:mongoose.Schema.Types.String,ref:"user"}
+	},
+	{ _id: false }
+);
+
+schema.plugin(autoIncrement, { id: "cartAutoIncrement", inc_field: "_id" });
+
+module.exports = mongoose.model("cart", schema);
+
