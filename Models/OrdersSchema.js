ff4f3d558742a0e64986c327ad0f9fe3003cd023
m7moud emam <m7moud_emam@hotmail.com>
2022-02-25 15:44:26 +0200

    Orders

--- Models/OrdersSchema.js
+++ Models/OrdersSchema.js
@@ -5,13 +5,17 @@ const AutoIncrement = require('mongoose-sequence')(mongoose);
 // 1 - build schema with validation 
 const schema = new mongoose.Schema({
     _id: Number,
-    user: { type: mongoose.Schema.Types.String, ref: "user" },
-    shipers: { type: Number, ref: "shipers" },
+    user: { type: String, ref: "user" },
+    shippers: { type: Number, ref: "shippers" },
     order_status: Number,
     tax: Number,
     payment: Number,
     order_date: Date,
-
+    product: [{
+        product: { type: Number, ref: "product" },
+        product_number: Number,
+        unit_price: Number,
+    }]
 }, { _id: false })
 
 schema.plugin(AutoIncrement, { id: "eventAutoIncrement", inc_field: "_id" });
