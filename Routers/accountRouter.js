0299762cfc8970159976f2a16a35f53cff77439b
KarimShahin <karim.alaa.shahin@gmail.com>
2022-02-28 00:49:45 +0200

    Account Router fix conflicts

--- Routers/accountRouter.js
+++ Routers/accountRouter.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const accountController = require('./../Controllers/accountController')
-const isAuth = require('./../middelWare/auth')
+const express = require("express");
+const router = express.Router();
+const accountController = require("./../Controllers/accountController");
+const isAuth = require("./../middelWare/auth");
 
-router.post('/', accountController.postUser) // Redirect
+router.post("/", accountController.postUser); // Redirect
 // router.post('/login', accountController.userLogin) // Just to generate token (useless)
-router.get('/', accountController.getAllUsers) // Admin get all users profile
-router.get('/me', isAuth, accountController.getProfile) // User get profile
-router.patch('/', isAuth, accountController.updateProfile) // User Update profile
-router.delete('/', isAuth, accountController.deleteProfile) // User Update profile
+router.get("/", accountController.getAllUsers); // Admin get all users profile
+router.get("/me", isAuth, accountController.getProfile); // User get profile
+router.patch("/", isAuth, accountController.updateProfile); // User Update profile
+router.delete("/", isAuth, accountController.deleteProfile); // User Update profile
 
 module.exports = router;
