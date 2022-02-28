24ceed0b413f1eb7194ee2c2e6fd1994c8e89649
KarimShahin <karim.alaa.shahin@gmail.com>
2022-02-28 17:11:08 +0200

    adding shippers in login & adding email to shipper

--- Controllers/authenticationController.js
+++ Controllers/authenticationController.js
@@ -6,29 +6,57 @@ const JWT = require("jsonwebtoken");
 
 exports.login = (request, response, next) => {
 	errorHandeler(request);
-	userModel
-		.findOne({ email: request.body.email })
-		.then((data) => {
-			if (data == null) {
-				shipperModel.findOne({ email: request.body.email }).then((data) => {
-					if (data == null) {
-						throw new Error("You are not in the system Go Out");
-					}
-				});
-			}
-			if (bcrypt.compareSync(request.body.password, data.password)) {
-				let token = JWT.sign(
-					{
-						email: request.body.email,
-						role: "user",
-					},
-					process.env.SECRET_KEY,
-					{ expiresIn: "2h" }
-				);
-				response.status(200).json({ message: `Welcome user`, data, token });
-			} else throw new Error("UserName or Password is not Correct");
-		})
-		.catch((error) => next(error));
+	if (request.body.email == process.env.ADMIN_EMAIL && request.body.password == process.env.ADMIN_PASSWORD) {
+		let token = JWT.sign(
+			{
+				email: request.body.email,
+				role: "admin",
+			},
+			process.env.SECRET_KEY,
+			{ expiresIn: "2h" }
+		);
+		response.status(200).json({ message: "welcome admin", token });
+	} else {
+		userModel
+			.findOne({ email: request.body.email })
+			.then((data) => {
+				if (data == null) {
+					shipperModel
+						.findOne({ email: request.body.email })
+						.then((data) => {
+							if (data == null) {
+								throw new Error("You are not in the system Go Out");
+							}
+							if (bcrypt.compareSync(request.body.password, data.password)) {
+								let token = JWT.sign(
+									{
+										email: request.body.email,
+										role: "shipper",
+									},
+									process.env.SECRET_KEY,
+									{ expiresIn: "2h" }
+								);
+								response.status(200).json({ message: "welcome Shipper", data, token });
+							} else {
+								throw new Error("Email or Password is not Correct");
+							}
+						})
+						.catch((error) => next(error));
+				}
+				if (bcrypt.compareSync(request.body.password, data.password)) {
+					let token = JWT.sign(
+						{
+							email: request.body.email,
+							role: "user",
+						},
+						process.env.SECRET_KEY,
+						{ expiresIn: "2h" }
+					);
+					response.status(200).json({ message: `Welcome user`, data, token });
+				} else throw new Error("Email or Password is not Correct");
+			})
+			.catch((error) => next(error));
+	}
 };
 
 exports.register = (request, response, next) => {
