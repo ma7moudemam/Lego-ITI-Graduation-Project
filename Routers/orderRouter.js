3d1b6e9189ffe51393deeeb9aae5eac43988f3b3
m7moud emam <m7moud_emam@hotmail.com>
2022-04-09 17:32:18 +0200

    fix order

--- Routers/orderRouter.js
+++ Routers/orderRouter.js
@@ -5,7 +5,15 @@ const { body, query, param } = require("express-validator");
 
 router
 	.route("/order")
-	.post(controller.addOrder)
+	.post([
+		body("user").isString().withMessage("user is required"),
+		body("shipper").number().withMessage("shipper is required"),
+		body("order_status").number().withMessage("order_status is required"),
+		body("tax").number().withMessage("tax is required"),
+		body("payment").number().withMessage("payment is required"),
+		body("order_date").isString().withMessage("order_date is required"),
+		body("product").isArray().withMessage("product is required"),
+	],controller.addOrder)
 	.get(controller.getOrder)
 	.get(controller.getAllOrders)
 	.put(controller.updateOrder)
