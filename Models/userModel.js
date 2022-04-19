831d625093e486a6b057ec64dbf56dc33af3bf52
Karim <karim.alaa.shahin@gmail.com>
2022-04-19 04:44:25 +0200

    update error message

--- Models/userModel.js
+++ Models/userModel.js
@@ -2,39 +2,40 @@ const mongoose = require("mongoose");
 const autoIncrement = require("mongoose-sequence")(mongoose);
 
 const schema = new mongoose.Schema(
-  {
-    _id: Number,
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    userName: {
-      type: String,
-    },
-    age: {
-      type: Number,
-      required: true,
-    },
-    country: { type: String, required: true },
-    address: {
-      city: { type: String },
-      street: { type: String },
-      building: { type: String },
-    },
-    wishlist: [
-      {
-        type: Number,
-        ref: "product",
-      },
-    ],
-    blocked: { type: Boolean }
-  },
-  { _id: false }
+	{
+		_id: Number,
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		userName: {
+			type: String,
+			required: true,
+		},
+		age: {
+			type: Number,
+			required: true,
+		},
+		country: { type: String, required: true },
+		address: {
+			city: { type: String },
+			street: { type: String },
+			building: { type: String },
+		},
+		wishlist: [
+			{
+				type: Number,
+				ref: "product",
+			},
+		],
+		blocked: { type: Boolean },
+	},
+	{ _id: false }
 );
 
 schema.plugin(autoIncrement, { id: "user-auto-increament", inc_field: "_id" });
